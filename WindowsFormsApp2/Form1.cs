a4ce44ee17cdc6c0a0fccf10abc80c2bd01be9c0
ishikawa <s-ishikawa_fk@jinearth.co.jp>
2023-05-31 07:22:50 +0900

    作成途中

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Security;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+public class OpenFileDialogForm : Form
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.EnableVisualStyles();
+        Application.Run(new OpenFileDialogForm());
+    }
+
+    private Button selectButton;
+    private OpenFileDialog openFileDialog1;
+
+    public OpenFileDialogForm()
+    {
+        openFileDialog1 = new OpenFileDialog()
+        {
+            FileName = "Select a text file",
+            Filter = "Text files (*.txt)|*.txt",
+            Title = "Open text file"
+        };
+
+        selectButton = new Button()
+        {
+            Size = new Size(100, 20),
+            Location = new Point(15, 15),
+            Text = "Select file"
+        };
+        selectButton.Click += new EventHandler(selectButton_Click);
+        Controls.Add(selectButton);
+    }
+
+    private void selectButton_Click(object sender, EventArgs e)
+    {
+        if (openFileDialog1.ShowDialog() == DialogResult.OK)
+        {
+            try
+            {
+                var filePath = openFileDialog1.FileName;
+                using (Stream str = openFileDialog1.OpenFile())
+                {
+                    Process.Start("notepad.exe", filePath);
+                }
+            }
+            catch (SecurityException ex)
+            {
+                MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                $"Details:\n\n{ex.StackTrace}");
+            }
+        }
+    }
+}
